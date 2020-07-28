fa34877a9949edc0e228075a8bb31d80e08e6139
lucasfrutig0 <lucasfrutig0@gmail.com>
2020-07-28 11:12:19 -0300

    initial setup project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Menu from "./components/Menu";
+import BannerMain from "./components/BannerMain";
+import Carousel from "./components/Carousel";
+import Footer from "./components/Footer";
+
+import dadosinicias from "./data/dados_iniciais.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "#141414" }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosinicias.categorias[0].videos[0].titulo}
+        url={dadosinicias.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-End? Trabalhado na área"}
+      />
+
+      <Carousel ignoreFirstVideo category={dadosinicias.categorias[0]} />
+      <Footer></Footer>
     </div>
   );
 }
